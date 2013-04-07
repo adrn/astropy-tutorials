53c9f606db80647d001bf8e97fd2b7072450042f
kelle <kellecruz@gmail.com>
2013-04-07 13:17:30 -0400

    worked on reading in FITS spectrum

--- tutorials/FITS-Spectrum/fits-spectrum.py
+++ tutorials/FITS-Spectrum/fits-spectrum.py
@@ -1,6 +1,4 @@
-# coding: utf-8
 
-"""  """
 
 from __future__ import division, print_function
 
@@ -12,39 +10,45 @@ import numpy as np
 from scipy.optimize import leastsq
 import emcee
 
+#Astropy
 from astropy import wcs
 from astropy.io import fits
 
-hdulist = fits.open("L5g_0355+11_Cruz09.fits")
-hdu = hdulist[0]
-w = wcs.WCS(hdu.header)
+data_file = 'L5g_0355+11_Cruz09.fits'
 
-flux = hdu.data
-wavelength = w.all_pix2world(np.arange(len(flux))[:,np.newaxis], 0)
-wavelength = wavelength.reshape(len(wavelength))
+hdulist = fits.open(data_file) #read in all HDUs/extensions
+#in this case, only one HDU
+hdu0 = hdulist[0]
+wcs0 = wcs.WCS(hdu0.header)
 
-fig,axes = subplots(2,1,figsize=(12,10))
-axes[0].plot(wavelength, flux, drawstyle="steps")
-axes[0].set_xlim(wavelength.min(), wavelength.max())
-axes[0].set_ylabel("Flux [{0}]".format(hdu.header["BUNIT"]), fontsize=20)
-axes[0].axvline(6708, color="red", linestyle="--")
-axes[0].xaxis.set_ticklabels([])
+#in this case, the only data in the FITS file is the flux values
+flux0 = hdu0.data
+n_pix0=len(flux0)
 
-# Zoomed in panel
-axes[1].plot(wavelength, flux, drawstyle="steps")
-axes[1].set_xlim(6650, 6800)
-axes[1].set_ylim(-0.001, 0.01)
-axes[1].set_xlabel(r"Wavelength [$\AA$]", fontsize=20)
-axes[1].set_ylabel("Flux [{0}]".format(hdu.header["BUNIT"]), fontsize=20)
+# create numpy array of integers from 0 to number of pixels as a row. then reshaped to be column. placeholder for wavelength. 
+# IDL equivalent = INTARR(n_elements(flux))
+pixel_grid = np.arange(0,n_pix0).reshape(n_pix0,1)
 
-fig.subplots_adjust(hspace=0.05)
+#make wavelength array with astropy.wcs magic via all_pix2world method
+wavelength0 = wcs0.all_pix2world(pixel_grid,0)
 
+#reshape to row
+wavelength0 = wavelength.reshape(n_pix0)
+
+##Now we have wavelength and flux for 0th HDU/extension.
+
+
+# measure EW of Lithium line.
 idx = (wavelength > 6650) & (wavelength < 6750)
 Li_flux = flux[idx]
 Li_wvln = wavelength[idx]
 
+# create lorentzian & gaussian functions
+# these should be hardcoded into astropy
 def gaussian(x, mu, sigma):
     return 1. / np.sqrt(2*np.pi) / sigma * np.exp(-(x-mu)**2 / (2*sigma**2))
+
+def laurentzian(x, x0, gamma)
     
 def model1(p, x):
     """ One Gaussian + straight line model """
@@ -62,6 +66,23 @@ def error_function2(p, x, y):
 
 p_opt, ier = leastsq(error_function1, [-0.01, 6700., 5., 0., 0.], args=(Li_wvln, Li_flux))
 p_opt2, ier = leastsq(error_function2, [-0.01, 6700., 5., 0, 6710., 5., 0., 0.], args=(Li_wvln, Li_flux))
+
+fig,axes = subplots(2,1,figsize=(12,10))
+axes[0].plot(wavelength, flux, drawstyle="steps")
+axes[0].set_xlim(wavelength.min(), wavelength.max())
+axes[0].set_ylabel("Flux [{0}]".format(hdu.header["BUNIT"]), fontsize=20)
+axes[0].axvline(6708, color="red", linestyle="--")
+axes[0].xaxis.set_ticklabels([])
+
+# Zoomed in panel
+axes[1].plot(wavelength, flux, drawstyle="steps")
+axes[1].set_xlim(6650, 6800)
+axes[1].set_ylim(-0.001, 0.01)
+axes[1].set_xlabel(r"Wavelength [$\AA$]", fontsize=20)
+axes[1].set_ylabel("Flux [{0}]".format(hdu.header["BUNIT"]), fontsize=20)
+
+fig.subplots_adjust(hspace=0.05)
+
 
 figure(figsize=(10,8))
 plot(Li_wvln, Li_flux, drawstyle="steps", color="k")
